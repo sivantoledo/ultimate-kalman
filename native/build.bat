c606db8230caaff2a26182a66f10d5e068f26368
Sivan Toledo <stoledo@tau.ac.il>
2024-04-24 15:06:22 +0300

    cleanup, hopefully final

--- native/build.bat
+++ native/build.bat
@@ -1,64 +1,67 @@
 setlocal
 
+ 
+@ECHO OFF
+	
+IF "%1"=="" (
+  ECHO provide an argument, the name of the C program you want to compile and run
+  GOTO :eof
+) 
+
+SET test=%1%
+
 @ECHO ON
 
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+REM call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
 call "C:\Program Files (x86)\Intel\oneAPI\setvars.bat" intel64
 
 ::echo %MKLROOT%
 ::echo %INCLUDE%
 
 :: possible mkl setvars args: ilp64
- 
-@ECHO ON
-	
-IF "%1"=="" (
-  ECHO no arguments, building everything
-  SET dll=true
-  SET test=true
-) 
 
-IF "%1"=="dll"       (SET dll=true) 
-IF "%1"=="test"      (SET test=true) 
 
 :: /Og rather than /Ox ?
 
-IF "%dll%"=="true" (
-  ECHO generating library dll
-  cl ^
-  /O2 /Ox /EHsc ^
-  -D_JNI_IMPLEMENTATION_ ^
-  -DINPROCESS ^
-  -I "%JAVA_HOME%\include" ^
-  -I "%JAVA_HOME%\include\win32" ^
-  -Fejni-sdcard.dll ^
-    sdcard\tau_atlas_logger_SDCardController.c  ^
-    /MD /LD
-  
-  COPY jni-sdcard.dll %ATLAS_DIST%\windows
-  ECHO generated sdcard dll
-)
+SET C_FLAGS=/Zc:__cplusplus /Zp8 /GR /W3 /EHs /nologo /MD /O2 /Oy- -DNDEBUG
+
+SET BLAS_LAPACK_FLAGS=-DBUILD_MKL
+SET BLAS_LAPACK_LIBS=mkl_intel_lp64_dll.lib mkl_intel_thread_dll.lib mkl_core_dll.lib libiomp5md.lib
+REM define MKL_ILP64 for 64-bit integers
 
 
-IF "%test%"=="true" (
-  ECHO generating test program
-  cl ^
-  -Ferotation.exe ^
+REM mkl_rt.lib 
+REM liblapack.lib libblas.lib
+
+set path=.;%path%
+
+REM -DBUILD_DEBUG_PRINTOUTS
+REM -DBUILD_BLAS_STRLEN_END
+REM -DBUILD_BLAS_UNDERSCORE
+
+REM echo %MKLROOT%
+
+ECHO generating test program %test%.exe
+
+cl ^
+  %C_FLAGS% ^
+  -Fe%test%.exe ^
   -I. ^
+  %BLAS_LAPACK_FLAGS% ^
   -DBUILD_WIN32_GETTIMEOFDAY ^
-  -DBUILD_MKL_H ^
-  rotation.c ^
   ultimatekalman.c ^
-  mkl_rt.lib ^
-  /MT
+  %test%.c ^
+  %BLAS_LAPACK_LIBS% 
 
+ECHO generated tes
 
-
+DEL *.obj
   
-  ECHO generated test
-)
+%test%.exe
 
-
-ECHO done
+ECHO done running test 
+  
+ECHO build script done
 
 
