844a4966f18cd58d11a73b7374fa26ae6b4b4c14
Sivan Toledo <stoledo@mail.tau.ac.il>
2024-04-24 17:36:39 +0300

    now same functionality as build.bat

--- native/build.sh
+++ native/build.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
+
+if [ "$#" -ne 1 ]; then
+    echo "provide an argument, the name of the C program you want to compile and run"
+    exit
+fi
+
+echo generating test program $1
+
 gcc \
+    -O2 \
     -DBUILD_BLAS_UNDERSCORE -DBUILD_LAPACK_UNDERSCORE \
     -DBUILD_DEBUG_PRINTOUTSx \
-    -o rotation \
-    rotation.c \
+    -o $1 \
+    $1.c \
     ultimatekalman.c \
-    -llapack -lblas -lm 
+    -llapack -lblas -lm
+
+echo generated test program
+
+./$1
+
+echo done running test
+
+echo build script done
+
 
