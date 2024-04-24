7235b68cfe2a1df9bac4d1787bad76c8ab49c6b3
Sivan Toledo <stoledo@tau.ac.il>
2024-04-24 17:45:38 +0300

    small improvement in Java 1.8 compatibility

--- build.bat
+++ build.bat
@@ -2,7 +2,8 @@ setlocal
 
 echo COMPLILING JAVA
 
-javac -cp commons-math3-3.6.1.jar -d bin -sourcepath src --target 1.8 --source 1.8 src\sivantoledo\kalman\*.java
+:: javac -cp commons-math3-3.6.1.jar -d bin -sourcepath src --target 1.8 --source 1.8 src\sivantoledo\kalman\*.java
+javac -cp commons-math3-3.6.1.jar -d bin -sourcepath src --release 8 src\sivantoledo\kalman\*.java
 
 echo CREATING JAR
 
