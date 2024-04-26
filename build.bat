4f38c271bb98d5a61c0cd2a55ddaa3b93cc961fa
Sivan Toledo <stoledo@tau.ac.il>
2024-04-26 09:01:14 +0300

    compile Rotation.java and better printouts

--- build.bat
+++ build.bat
@@ -1,9 +1,17 @@
 setlocal
 
+@echo OFF
+
 echo COMPLILING JAVA
 
 :: javac -cp commons-math3-3.6.1.jar -d bin -sourcepath src --target 1.8 --source 1.8 src\sivantoledo\kalman\*.java
-javac -cp commons-math3-3.6.1.jar -d bin -sourcepath src --release 8 src\sivantoledo\kalman\*.java
+javac ^
+  -cp commons-math3-3.6.1.jar ^
+  -d bin ^
+  -sourcepath src ^
+  --release 8 ^
+  src\sivantoledo\kalman\*.java ^
+  src\sivantoledo\kalman\tests\Rotation.java 
 
 echo CREATING JAR
 
@@ -16,7 +24,9 @@ cd bin
 jar cvfm ..\ultimatekalman.jar ..\MANIFEST.TXT sivantoledo\* 
 cd ..
 
+echo RUNNING JAVA TEST
 java -cp ultimatekalman.jar;commons-math3-3.6.1.jar;bin sivantoledo.kalman.tests.Rotation 
+echo DONE RUNNING JAVA TEST
 
 
 
