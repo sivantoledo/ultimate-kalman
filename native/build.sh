dae1f19d828b651f65e99d8ab0613278644e268d
Sivan Toledo <stoledo@mail.tau.ac.il>
2024-06-09 09:49:06 +0300

    build rotation if no argument is given

--- native/build.sh
+++ native/build.sh
@@ -2,24 +2,25 @@
 
 
 if [ "$#" -ne 1 ]; then
-    echo "provide an argument, the name of the C program you want to compile and run"
-    exit
+    test=rotation
+else
+    test=$1
 fi
 
-echo generating test program $1
+echo generating test program $test
 
 gcc \
     -O2 \
     -DBUILD_BLAS_UNDERSCORE -DBUILD_LAPACK_UNDERSCORE \
     -DBUILD_DEBUG_PRINTOUTSx \
-    -o $1 \
-    $1.c \
+    -o $test \
+    $test.c \
     ultimatekalman.c \
     -llapack -lblas -lm
 
 echo generated test program
 
-./$1
+./$test
 
 echo done running test
 
