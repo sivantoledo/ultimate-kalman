ee2078e08e9c3c65d950727388655398af86690a
Sivan Toledo <stoledo@tau.ac.il>
2025-03-13 08:13:50 +0200

    fixed void** types

--- c/parallel_sequential.c
+++ c/parallel_sequential.c
@@ -7,11 +7,11 @@
 void parallel_set_thread_limit(int number_of_threads) {}
 void parallel_set_blocksize   (int blocksize_in)      {}
 
-void foreach_in_range(void (*func)(void**, int, size_t, size_t), void** array, int length, size_t n) {
+void foreach_in_range(void (*func)(void*, int, size_t, size_t), void* array, int length, size_t n) {
 	(*func)( array, length, 0, n );
 }
 
-void foreach_in_range_two(void (*func)(void**, void**, int, size_t, size_t), void** array1, void** array2, int length, size_t n) {
+void foreach_in_range_two(void (*func)(void*, void**, int, size_t, size_t), void* array1, void* array2, int length, size_t n) {
 	(*func)( array1, array2, length, 0, n );
 }
 
