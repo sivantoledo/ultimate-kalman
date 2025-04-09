f06d95cf9ecd5b4b8c94138b45bba8935d1cbce0
Sivan Toledo <stoledo@tau.ac.il>
2025-04-09 18:28:49 +0300

    systematic testing

--- matlab/examples/compare.m
+++ matlab/examples/compare.m
@@ -1,4 +1,4 @@
-function t = compare(kalman1, kalman2, seed, n, m, count, filter, smooth)
+function [fse,fce,sse,sce] = compare(kalman1, kalman2, seed, n, m, count, filter, smooth)
 % COMPARE a function to compare the outputs of two Kalman filters/smoothers
 %         mainly to test correctness of new implementations and variants.
 %
@@ -14,16 +14,135 @@ function t = compare(kalman1, kalman2, seed, n, m, count, filter, smooth)
 %
 % copyright 2024 Sivan Toledo
 
+if ~isscalar(seed)
+    test = seed;
+    cov1type = n;
+    cov2type = m;
+
+    fse = NaN;
+    fce = NaN;
+    sse = NaN;
+    sce = NaN;
+
+
+    for i=1:size(test,1)
+        n = test(i,1);
+        l = test(i,2);
+        m = test(i,3);
+        f = test(i,4);
+        r = test(i,5);
+
+        if i==1
+            kalman1.evolve(n);
+            kalman2.evolve(n);
+        else
+            H = eye(l,n);
+            [F,~] = qr( randn(max(l,n_i_m_1),max(l,n_i_m_1)) );
+            F = F(1:l,1:n_i_m_1);
+            c = randn(l,1);
+            [K1,K2] = cov(l,cov1type,cov2type);
+            %kalman1.evolve(n,H,F,c,CovarianceMatrix(Ke, 'C'))
+            kalman1.evolve(n,H,F,c,K1)
+            %kalman1.evolve(n,H,F,c,CovarianceMatrix(eye(l), 'C'))
+            %kalman2.evolve(n,H,F,c,CovarianceMatrix(Ke, 'C'))
+            kalman2.evolve(n,H,F,c,K2)
+            %kalman2.evolve(n,H,F,c,CovarianceMatrix(eye(l), 'C'))
+        end
+
+        if m>0
+            G = randn(m,n);
+            o = randn(m,1);
+            [C1,C2] = cov(m,cov1type,cov2type);
+            %%Cf = randn(m,m);
+            %%Ce = Cf' * Cf;
+            %%L=chol(Ce,'lower');
+            %%W=inv(L);
+            %kalman1.observe(G,o,CovarianceMatrix(Ce, 'C'));
+            kalman1.observe(G,o,C1);
+            %kalman1.observe(G,o,CovarianceMatrix(eye(m), 'C'));
+            %kalman2.observe(G,o,CovarianceMatrix(Ce, 'C'));
+            kalman2.observe(G,o,C2);
+            %kalman2.observe(G,o,CovarianceMatrix(eye(m), 'C'));
+        else
+            kalman1.observe([],[],CovarianceMatrix([],cov1type));
+            kalman2.observe([],[],CovarianceMatrix([],cov1type));
+        end
+
+        n_i_m_1 = n;
+
+        [u1,Z1] = kalman1.estimate();
+        [u2,Z2] = kalman2.estimate();
+
+        filteredStateErr(i) = max(abs((u1-u2)./u1));
+        %filteredCovErr(1)   = max(max(abs((Z1.explicit()-Z2.explicit())./Z1.explicit())));
+        filteredCovNorm       = max(max(abs(Z1.explicit())));
+        filteredCovEltErr(i)   = max(max(abs((Z1.explicit()-Z2.explicit())./Z1.explicit())));
+        filteredCovNrmErr(i)   = max(max(abs((Z1.explicit()-Z2.explicit())/filteredCovNorm)));
+
+        if  isnan(sum(u1)) &&  isnan(sum(u2)); filteredStateErr(i)=0; filteredCovNrmErr(i)=0; end
+        if  isnan(sum(u1)) && ~isnan(sum(u2)); filteredStateErr(i)=inf; filteredCovNrmErr(i)=inf; end
+        if ~isnan(sum(u1)) &&  isnan(sum(u2)); filteredStateErr(i)=inf; filteredCovNrmErr(i)=inf; end
+
+        %if filteredStateErr(i) > 1e-8
+        %    i-1
+        %    [u1 u2]
+        %    error('large error');
+        %end
+    end
+
+    %filteredStateErr
+    fse = max(filteredStateErr);
+    fce = max(filteredCovNrmErr);
+    fprintf('max filtered relative error %.2e (elementwise) max filtered covariance error %.2e (normwise)\n', fse, fce);
+
+    kalman1.smooth();
+    kalman2.smooth();
+
+
+    for i=1:size(test,1)
+
+        [u1,Z1] = kalman1.estimate(i-1);
+        [u2,Z2] = kalman2.estimate(i-1);
+
+        smoothedStateErr(i) = max(abs((u1-u2)./u1));
+        %filteredCovErr(1)   = max(max(abs((Z1.explicit()-Z2.explicit())./Z1.explicit())));
+        smoothedCovNorm       = max(max(abs(Z1.explicit())));
+        smoothedCovEltErr(i)   = max(max(abs((Z1.explicit()-Z2.explicit())./Z1.explicit())));
+        smoothedCovNrmErr(i)   = max(max(abs((Z1.explicit()-Z2.explicit())/filteredCovNorm)));
+
+        if  isnan(sum(u1)) &&  isnan(sum(u2)); smoothedStateErr(i)=   0; smoothedCovNrmErr(i)=   0; end
+        if  isnan(sum(u1)) && ~isnan(sum(u2)); smoothedStateErr(i)= inf; smoothedCovNrmErr(i)= inf; end
+        if ~isnan(sum(u1)) &&  isnan(sum(u2)); smoothedStateErr(i)= inf; smoothedCovNrmErr(i)= inf; end
+    end
+
+    sse = max(smoothedStateErr);
+    sce = max(smoothedCovNrmErr);
+    fprintf('max smoothed relative error %.2e (elementwise) max smoothed covariance error %.2e (normwise)\n', fse, fce);
+
+    return
+end
+
 rng(seed);
 
 H = eye(n);
 [F,~] = qr( randn(n,n), 0 );
-[G,~] = qr( randn(m,n), 0 );
+%if m >= n
+%    [G,~] = qr( randn(m,n), 0 );
+%else
+%    [G,~] = qr( randn(m,n) )
+%end
+G = randn(m,n);
 c = zeros(n,1);
 o = randn(m,1);
 
-K   = CovarianceMatrix(eye(n), 'C');
-C   = CovarianceMatrix(eye(m), 'C');
+Kf = randn(n,n);
+Ke = Kf' * Kf;
+%K   = CovarianceMatrix(eye(n), 'C');
+K   = CovarianceMatrix(Ke, 'C');
+Cf = randn(m,m);
+Ce = Cf' * Cf;
+%C   = CovarianceMatrix(eye(m), 'C');
+C   = CovarianceMatrix(Ce, 'C');
 
 % cause lazy factorization
 K.weigh(ones(n,1));
@@ -51,6 +170,14 @@ if filter
 end
 
 for i=1:count
+
+    if true
+        [F,~] = qr( randn(n,n), 0 );
+        G = randn(m,n);
+        c = zeros(n,1);
+        o = randn(m,1);
+    end
+
     kalman1.evolve(n,H,F,c,K);
     kalman2.evolve(n,H,F,c,K);
 
@@ -68,6 +195,10 @@ for i=1:count
         filteredCovNorm       = max(max(abs(Z1.explicit())));
         filteredCovEltErr(i+1)   = max(max(abs((Z1.explicit()-Z2.explicit())./Z1.explicit())));
         filteredCovNrmErr(i+1)   = max(max(abs((Z1.explicit()-Z2.explicit())/filteredCovNorm)));
+        %if filteredCovEltErr(i+1) > 1e-2
+        %    fprintf("large error in covariance matrices of filtered estimates for step %d (%.2e)\n",i,filteredCovEltErr(i+1));
+        %    [Z1.explicit() Z2.explicit()]
+        %end
     end
 end
 
@@ -84,9 +215,16 @@ if smooth
         smoothedCovEltErr(i+1)   = max(max(abs((Z1.explicit()-Z2.explicit())./Z1.explicit())));
         smoothedCovNrmErr(i+1)   = max(max(abs((Z1.explicit()-Z2.explicit())/smoothedCovNorm)));
 
+        %if smoothedCovEltErr(i+1) > 1e-2
+        %    fprintf("large error in covariance matrices of smoothed estimates for step %d (%.2e)\n",i,smoothedCovEltErr(i+1));
+        %    [Z1.explicit() Z2.explicit()]
+        %end
+        %[Z1.explicit() Z2.explicit()]
         %if smoothedCovNrmErr(i+1) > 1e-12
-        %    z1e=Z1.explicit()
-        %    z2e=Z2.explicit()
+        %    fprintf('err in %d (%.2e)\n',i+1,smoothedCovNrmErr(i+1))
+        %z1e=Z1.explicit();
+        %z2e=Z2.explicit();
+        %[z1e z2e z1e-z2e]
         %end
     end
 end
@@ -108,7 +246,7 @@ if filter
     title('Filtered Covariance Errors');
 
     fprintf('max filtered relative error %.2e (elementwise) max filtered covariance error %.2e (elementwise) %.2e (normwise)\n' ...
-           ,max(filteredStateErr), max(filteredCovEltErr), max(filteredCovNrmErr));
+        ,max(filteredStateErr), max(filteredCovEltErr), max(filteredCovNrmErr));
 end
 
 if smooth
@@ -125,5 +263,56 @@ if smooth
     title('Smoothed Covariance Errors');
 
     fprintf('max smoothed relative error %.2e (elementwise) max filtered covariance error %.2e (elementwise) %.2e (normwise)\n' ...
-           ,max(smoothedStateErr), max(smoothedCovEltErr), max(smoothedCovNrmErr));
+        ,max(smoothedStateErr), max(smoothedCovEltErr), max(smoothedCovNrmErr));
+end
+
+    function [cov1,cov2] = cov(d,cov1type,cov2type)
+        if cov1type=='w' || cov2type=='w'
+            covdiag = true;
+        else
+            covdiag = false;
+        end
+
+        clear cov1 cov2;
+        if covdiag
+            w   = randn(d,1);
+            cov = diag(w.^-2);
+            W   = diag(w);
+            if cov1type=='W'
+                cov1=CovarianceMatrix(W, 'W');
+            end
+            if cov1type=='w'
+                cov1=CovarianceMatrix(w, 'w');
+            end
+            if cov1type=='C'
+                cov1=CovarianceMatrix(cov, 'C');
+            end
+            if cov2type=='W'
+                cov2=CovarianceMatrix(W, 'W');
+            end
+            if cov2type=='w'
+                cov2=CovarianceMatrix(w, 'w');
+            end
+            if cov2type=='C'
+                cov2=CovarianceMatrix(cov, 'C');
+            end
+        else
+            fact = randn(d,d);
+            cov  = fact' * fact;
+            L=chol(cov,'lower');
+            W=inv(L);
+            if cov1type=='W'
+                cov1=CovarianceMatrix(W, 'W');
+            end
+            if cov1type=='C'
+                cov1=CovarianceMatrix(cov, 'C');
+            end
+            if cov2type=='W'
+                cov2=CovarianceMatrix(W, 'W');
+            end
+            if cov2type=='C'
+                cov2=CovarianceMatrix(cov, 'C');
+            end
+        end
+    end
 end
---
+++ matlab/examples/kalmanTest.m
@@ -0,0 +1,212 @@
+rng(1)
+
+% k = KalmanUltimate();
+% k = KalmanNative(struct('algorithm','Ultimate'));
+% n = 3;
+% k.evolve(n);
+% Cf = [ 4 -1 -1
+%        -1 3 -1
+%        -1 -1 3]
+% L = chol(Cf,'lower')
+% W = inv(L)
+% CC = CovarianceMatrix(Cf,'C');
+% CW = CovarianceMatrix(W,'W');
+% k.observe(eye(n),[1 2 3]',CC);
+% [e,c] = k.estimate();
+% e
+% c
+% W=c.W
+% inv(W'*W)
+% c.C
+% return
+
+
+NONE          = 0;
+UNOBSERVABALE = 1;     
+RECT_H        = 2;    
+LIMITATION_PLURAL = 4; % a restriction: only works on k>1    
+
+msparse = { 'msparse', kalmanFactory('KalmanSparse'),                                                bitor( UNOBSERVABALE, RECT_H) };
+mconv   = { 'mconv',   kalmanFactory('KalmanConventional'),                                          0 };
+nconv   = { 'nconv',   kalmanFactory('KalmanNative',struct('algorithm','Conventional')),             0 };
+pconv   = { 'pconv',   kalmanFactory('KalmanPython',struct('algorithm','KalmanConventional')),       0 };
+multps  = { 'multps',  kalmanFactory('KalmanUltimate',struct('estimateCovariance','PaigeSaunders')), bitor( UNOBSERVABALE, RECT_H) };
+pultps  = { 'pultps',  kalmanFactory('KalmanPython',struct('algorithm','KalmanUltimate')),   bitor( UNOBSERVABALE, RECT_H) };
+multsi  = { 'multsi',  kalmanFactory('KalmanUltimate',struct('estimateCovariance','SelInv')),        bitor( UNOBSERVABALE, RECT_H) };
+nultps  = { 'nultps',  kalmanFactory('KalmanNative',struct('algorithm','Ultimate')),                 bitor( UNOBSERVABALE, RECT_H) };
+jultps  = { 'jultps',  kalmanFactory('KalmanJava'),                                                  bitor( UNOBSERVABALE, RECT_H) };
+moddevn = { 'moddevn', kalmanFactory('KalmanOddevenSmoother'),                                       bitor(bitor( UNOBSERVABALE, RECT_H), LIMITATION_PLURAL) };
+massoc  = { 'massoc',  kalmanFactory('KalmanAssociativeSmoother'),                                   LIMITATION_PLURAL  };
+
+variants = { msparse
+             moddevn
+             massoc
+             multps
+             nultps 
+             mconv
+             nconv
+           };
+
+variants = { msparse
+             multps
+             multsi
+             pultps
+             nultps
+             mconv
+             pconv
+             nconv
+             moddevn
+             massoc
+           };
+
+tests = { uniform(1,1,1,1)
+          uniform(1,1,1,7)
+          uniform(2,2,2,1)
+          uniform(2,2,2,7)
+          uniform(3,3,3,1)
+          uniform(3,3,3,4)
+          uniform(3,3,3,5)
+          uniform(3,3,1,3) 
+          uniform(4,4,2,2) 
+          uniform(2,1,2,9)
+          uniform(3,2,3,9)
+          uniform(3,1,3,9)
+          [3 0 2 nan nan ; 3 1 2 nan nan ; 3 1 3 nan nan ; 3 1 2 nan nan ] % note the state with 3 observation in the middle; it's necessary
+          [1 1 0 nan nan ; 1 1 2 nan nan   ] % increasing state dimension
+          [1 1 0 nan nan ; 1 1 1 nan nan ; 2 1 2 nan nan ; 3 2 3 nan nan ; 3 3 0 nan nan ; 3 2 3 nan nan ; 4 2 3 nan nan] % increasing state dimension
+        }
+
+
+%          uniform(2,1,1,9)
+
+n = length(variants)-1;
+m = size(tests,1);
+
+FS=NaN*ones(n,m);
+FC=NaN*ones(n,m);
+SS=NaN*ones(n,m);
+SC=NaN*ones(n,m);
+
+v={};
+
+for i=1:n
+    for j=1:m
+        %[i j]
+        kalman1 = variants{1}{2}();
+        kalman2 = variants{i+1}{2}();
+        capabilities1 = variants{1}{3};
+        capabilities2 = variants{i+1}{3};
+        %class(kalman1)
+        %class(kalman2)
+        test = tests{j,1};
+        if rect_h(test)     && ~bitand(capabilities1,RECT_H       ); continue; end
+        if observable(test) && ~bitand(capabilities1,UNOBSERVABALE); continue; end
+        if rect_h(test)     && ~bitand(capabilities2,RECT_H       ); continue; end
+        if observable(test) && ~bitand(capabilities2,UNOBSERVABALE); continue; end
+        if ~plural(test)     && bitand(capabilities2,LIMITATION_PLURAL); continue; end
+        test
+        v{i,1}=variants{i+1}{1}
+        %plural(test)
+        [fse,fce,sse,sce] = compare(kalman1, kalman2, test, 'W', 'W');
+        if isfinite(fse) && fse  > 1e-8; FS(i,j) = 0; end 
+        if isfinite(fce) && fce  > 1e-8; FC(i,j) = 0; end 
+        if isfinite(sse) && sse  > 1e-8; SS(i,j) = 0; end 
+        if isfinite(sce) && sce  > 1e-8; SC(i,j) = 0; end 
+        if isfinite(fse) && fse <= 1e-8; FS(i,j) = 1; end 
+        if isfinite(fce) && fce <= 1e-8; FC(i,j) = 1; end 
+        if isfinite(sse) && sse <= 1e-8; SS(i,j) = 1; end 
+        if isfinite(sce) && sce <= 1e-8; SC(i,j) = 1; end 
+        if ~isfinite(fse); FS(i,j) = fse; end 
+        if ~isfinite(fce); FC(i,j) = fce; end 
+        if ~isfinite(sse); SS(i,j) = sse; end 
+        if ~isfinite(sce); SC(i,j) = sce; end 
+    end
+    
+end
+
+v
+FS
+FC
+SS
+SC
+
+%%% test covariance matrix types
+
+test = uniform(9,9,11,17);
+test = uniform(1,1,1,17);
+covTypes = { 'W', 'C', 'w' }
+
+n = length(variants);
+m = length(covTypes);
+
+FS=NaN*ones(n,m);
+FC=NaN*ones(n,m);
+SS=NaN*ones(n,m);
+SC=NaN*ones(n,m);
+
+v={};
+
+for i=1:n
+    for j=1:m
+        %[i j]
+        kalman1 = multps{2}();
+        kalman2 = variants{i}{2}();
+        v{i,1}=variants{i}{1}
+        [fse,fce,sse,sce] = compare(kalman1, kalman2, test, 'W', covTypes{j});
+        if isfinite(fse) && fse  > 1e-8; FS(i,j) = 0; end 
+        if isfinite(fce) && fce  > 1e-8; FC(i,j) = 0; end 
+        if isfinite(sse) && sse  > 1e-8; SS(i,j) = 0; end 
+        if isfinite(sce) && sce  > 1e-8; SC(i,j) = 0; end 
+        if isfinite(fse) && fse <= 1e-8; FS(i,j) = 1; end 
+        if isfinite(fce) && fce <= 1e-8; FC(i,j) = 1; end 
+        if isfinite(sse) && sse <= 1e-8; SS(i,j) = 1; end 
+        if isfinite(sce) && sce <= 1e-8; SC(i,j) = 1; end 
+        if ~isfinite(fse); FS(i,j) = fse; end 
+        if ~isfinite(fce); FC(i,j) = fce; end 
+        if ~isfinite(sse); SS(i,j) = sse; end 
+        if ~isfinite(sce); SC(i,j) = sce; end 
+    end
+    
+end
+
+covTypes
+v
+FS
+FC
+SS
+SC
+
+function prop = rect_h(t)
+    if sum(t(:,1) > t(:,2)) > 0 % n > l for some row
+        prop = true;
+    else
+        prop = false;
+    end
+end
+
+function prop = observable(t)
+    if sum(t(1,1) > t(1,3)) > 0 % n > m in first row
+        prop = true;
+    else
+        prop = false;
+    end
+end
+
+function prop = plural(t)
+    if size(t,1) > 1 % more than 1 step
+        prop = true;
+    else
+        prop = false;
+    end
+end
+
+
+function t = uniform(n,l,m,k)
+t = ones(k,1) * [ n l m nan nan];
+end
+
+function t = predict(n,l,m,k)
+t = ones(k,1) * [ n l 0 nan nan];
+t(1,:) = [ n 0 m nan nan ];
+end
+
